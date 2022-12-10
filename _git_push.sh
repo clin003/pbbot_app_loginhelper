e9360e1ee065b55f9dfaadd960c06c8465df42b1
白菜林 <noreply@3ae.cn>
2022-12-10 16:37:54 +0800

    v0.0.6 debug

--- _git_push.sh
+++ _git_push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=0.0.5
+VERSION=0.0.6
 APPNAME=pbbot_app_loginhelper
 echo "package constvar" > ./pkg/constvar/version.go
 echo "const(APP_NAME = \"${APPNAME}\"" >> ./pkg/constvar/version.go
--- pkg/constvar/version.go
+++ pkg/constvar/version.go
@@ -2,5 +2,5 @@ package constvar
 
 const (
 	APP_NAME    = "pbbot_app_loginhelper"
-	APP_VERSION = "0.0.5"
+	APP_VERSION = "0.0.6"
 )
